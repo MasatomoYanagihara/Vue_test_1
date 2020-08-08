688426ebb1587f6a57d05dca7c19d22cba51feb4
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-08 22:50:10 +0900

    POST通信テスト（Firestore）

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,59 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム：</label>
+    <input id="name" type="text" v-model="name" />
+    <br />
+    <br />
+    <label for="coment">コメント：</label>
+    <textarea id="comment" v-model="comment"></textarea>
+    <br />
+    <br />
+    <button @click="createComment">コメントをサーバーに送る</button>
+    <h2>掲示板</h2>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      name: "",
+      comment: ""
+    };
+  },
+
+  methods: {
+    createComment() {
+      axios
+        .post(
+          "https://firestore.googleapis.com/v1/projects/プロジェクトID/databases/(default)/documents/comments",
+          {
+            fields: {
+              name: {
+                stringValue: this.name
+              },
+              comment: {
+                stringValue: this.comment
+              }
+            }
+          }
+        )
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      this.name = "";
+      this.comment = "";
+    }
+  }
+};
+</script>
+
 
 <style>
 #app {
