e946b1e70825ace8c0112a367c5246818a7fd4b0
Masatomo Yanagihara <masatomo.yanagihara.jp@gmail.com>
2020-08-13 18:45:40 +0900

    LikeHeaderコンポーネントをローカル登録して使用。

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
-  <LikeNumber></LikeNumber>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+  </div>
 </template>
+
+<script>
+import LikeHeader from "./LikeHeader"; // LikeHeaderコンポーネントをローカル登録する為にimportする。
+
+export default {
+  components: {
+    /* 
+    LikeHeaderコンポーネントをローカル登録。
+    LikeHeader: LikeHeader;だがES6ではキーとバリューが同じなら省略して書ける。
+    */
+    LikeHeader
+  }
+};
+</script>
